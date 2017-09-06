29185c426a3e97aa0ae55d31db248903e4a3f683
AdrianaTR80 <31289704+AdrianaTR80@users.noreply.github.com>
2017-09-06 13:07:38 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import numpy as np
+import matplotlib as np
 import pylab as pl
-x=(6000,7000,8000,9000,10000)
-y=(73,80,85,87,89)
-pl.plot(x,y)
-x1=(7000,8000,9000,10000,11000)
-y2=(80,83,85,86,88)
-pl.plot(x1,y2)
-pl.xlabel('Voltaje (V)')
-pl.ylabel('Eficiencia (%)')
-pl.savefig('templ.png')
+pl.plot ((1,2,3,4),(9.00,8.66,8.40,7.50),'k', linewidth=1.0)
+pl.annotate('9.00', xy =(1,9.00), xytext=(0.9,8.5), color='b')
+pl.annotate('8.66', xy =(2,8.66), xytext=(1.9,9), color='b')
+pl.annotate('8.40', xy =(3,8.40), xytext=(2.9,8.0), color='b')
+pl.annotate('7.50', xy =(4,7.50), xytext=(3.9,8.0), color='b')
+pl.ylabel('promedio')
+pl.xlabel('semestre')
+pl.title ('Promedio por semestre')
+pl.axis((0.5,6.5,5.5,10))
+pl.grid(True)
+
+pl.savefig('graph.png')
